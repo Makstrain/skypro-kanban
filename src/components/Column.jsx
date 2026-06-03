f3e6e507c12203b37c36002c2d357e421729ff21
Maksim Ivanichenko <Maxim.Ivanichenko@atom.team>
2026-06-03 09:05:19 +0300

    Initial commit: React Kanban app

---
+++ src/components/Column.jsx
@@ -0,0 +1,25 @@
+import Card from "./Card";
+
+function Column({ title, cards }) {
+  return (
+    <div className="main__column column">
+      <div className="column__title">
+        <p>{title}</p>
+      </div>
+      <div className="cards">
+        {cards.map((card, index) => (
+          <div className="cards__item" key={index}>
+            <Card
+              title={card.title}
+              theme={card.theme}
+              date={card.date}
+              themeClass={card.themeClass}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Column;
